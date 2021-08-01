f4164934db059fab86efda8b07a654928c36a27a
TOUNSI Rania <rania.tounsi@ouidou.fr>
2021-08-01 14:42:11 +0200

    Design Project section

--- src/Main/Sections/Section2.js
+++ src/Main/Sections/Section2.js
@@ -1,6 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBuilding, faClock, faFileCode, faPeopleArrows, faStickyNote } from '@fortawesome/free-solid-svg-icons'
-
+import { BiFace } from "react-icons/bi";
+import { BsClockHistory, BsBuilding } from "react-icons/bs";
+import { AiOutlineCodepen } from "react-icons/ai";
+import { BiCodeAlt } from "react-icons/bi";
 function Section2(props) {
     
 
@@ -8,44 +11,44 @@ function Section2(props) {
         <section id="section2" className="section2">
             <div className="containerProject">
                 <div className="projectDescription container">
-                    <h2><FontAwesomeIcon icon={faPeopleArrows} /> ROLE:</h2> Alternante développeur web
-                    <h2><FontAwesomeIcon icon={faBuilding} />  ENTREPRISE:</h2> Ouidou Consulting, Paris
-                    <h2><FontAwesomeIcon icon={faClock}/>  PERIODE :</h2> 
+                    <h2><BiFace color="#f6bd60"/> ROLE:</h2> Alternante développeur web
+                    <h2><BsBuilding color="#f6bd60"/>  ENTREPRISE:</h2> Ouidou Consulting, Paris
+                    <h2><BsClockHistory color="#f6bd60"/>  PERIODE :</h2> 
                     
                 </div>
                 <div  className="missions">
-                    <h2><FontAwesomeIcon icon={faFileCode}/>  MISSIONS :</h2>
-                    <p>Développement d'une application web de gestion d'équipe avec : <b>React, Typescript, SpringBoot.</b></p>
-                    <p>Développement et Maintenance d'un logiciel en :  <b>React, NodeJs, SailsJs et PostgresSQL.</b></p>
-                    <p>Projet d'automatisation avec la technologie <b>RPA</b> en utilisant les frameworks UiPath et Microsoft Power Automate.</p>
+                    <h2><AiOutlineCodepen color="#f6bd60"/>  MISSIONS :</h2>
+                    <p><BiCodeAlt /> Développement d'une application web de gestion d'équipe avec : <b>React, Typescript, SpringBoot.</b></p>
+                    <p><BiCodeAlt /> Développement et Maintenance d'un logiciel en :  <b>React, NodeJs, SailsJs et PostgresSQL.</b></p>
+                    <p><BiCodeAlt /> Projet d'automatisation avec la technologie <b>RPA</b> en utilisant les frameworks UiPath et Microsoft Power Automate.</p>
                 </div>
             </div>
             <div className="containerProject">
                 <div className="projectDescription container">
-                    <h2><FontAwesomeIcon icon={faPeopleArrows} /> ROLE:</h2> Alternante développeur web
-                    <h2><FontAwesomeIcon icon={faBuilding} />  ENTREPRISE:</h2> Ouidou Consulting, Paris
-                    <h2><FontAwesomeIcon icon={faClock}/>  PERIODE :</h2> 
+                    <h2><BiFace color="#f6bd60"/> ROLE:</h2> Alternante développeur web
+                    <h2><BsBuilding color="#f6bd60"/>  ENTREPRISE:</h2> Ouidou Consulting, Paris
+                    <h2><BsClockHistory color="#f6bd60"/>  PERIODE :</h2> 
                     
                 </div>
                 <div  className="missions">
-                    <h2><FontAwesomeIcon icon={faFileCode}/>  MISSIONS :</h2>
-                    <p>Développement d'une application web de gestion d'équipe avec : <b>React, Typescript, SpringBoot.</b></p>
-                    <p>Développement et Maintenance d'un logiciel en :  <b>React, NodeJs, SailsJs et PostgresSQL.</b></p>
-                    <p>Projet d'automatisation avec la technologie <b>RPA</b> en utilisant les frameworks UiPath et Microsoft Power Automate.</p>
+                    <h2><AiOutlineCodepen color="#f6bd60"/>  MISSIONS :</h2>
+                    <p><BiCodeAlt /> Développement d'une application web de gestion d'équipe avec : <b>React, Typescript, SpringBoot.</b></p>
+                    <p><BiCodeAlt /> Développement et Maintenance d'un logiciel en :  <b>React, NodeJs, SailsJs et PostgresSQL.</b></p>
+                    <p><BiCodeAlt /> Projet d'automatisation avec la technologie <b>RPA</b> en utilisant les frameworks UiPath et Microsoft Power Automate.</p>
                 </div>
             </div>
             <div className="containerProject">
                 <div className="projectDescription container">
-                    <h2><FontAwesomeIcon icon={faPeopleArrows} /> ROLE:</h2> Alternante développeur web
-                    <h2><FontAwesomeIcon icon={faBuilding} />  ENTREPRISE:</h2> Ouidou Consulting, Paris
-                    <h2><FontAwesomeIcon icon={faClock}/>  PERIODE :</h2> 
+                    <h2><BiFace color="#f6bd60"/> ROLE:</h2> Alternante développeur web
+                    <h2><BsBuilding color="#f6bd60"/>  ENTREPRISE:</h2> Ouidou Consulting, Paris
+                    <h2><BsClockHistory color="#f6bd60"/>  PERIODE :</h2> 
                     
                 </div>
                 <div  className="missions">
-                    <h2><FontAwesomeIcon icon={faFileCode}/>  MISSIONS :</h2>
-                    <p>Développement d'une application web de gestion d'équipe avec : <b>React, Typescript, SpringBoot.</b></p>
-                    <p>Développement et Maintenance d'un logiciel en :  <b>React, NodeJs, SailsJs et PostgresSQL.</b></p>
-                    <p>Projet d'automatisation avec la technologie <b>RPA</b> en utilisant les frameworks UiPath et Microsoft Power Automate.</p>
+                    <h2><AiOutlineCodepen color="#f6bd60"/>  MISSIONS :</h2>
+                    <p><BiCodeAlt /> Développement d'une application web de gestion d'équipe avec : <b>React, Typescript, SpringBoot.</b></p>
+                    <p><BiCodeAlt /> Développement et Maintenance d'un logiciel en :  <b>React, NodeJs, SailsJs et PostgresSQL.</b></p>
+                    <p><BiCodeAlt /> Projet d'automatisation avec la technologie <b>RPA</b> en utilisant les frameworks UiPath et Microsoft Power Automate.</p>
                 </div>
             </div>
             
